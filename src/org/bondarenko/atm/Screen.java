187c19e9fbe1bc9bddad0299f3dd4985d5b7f1cb
Bondarenko Katya <karameilka@ukr.net>
2013-11-04 10:53:14 +0200

    Screen methods ware update

--- src/org/bondarenko/atm/Screen.java
+++ src/org/bondarenko/atm/Screen.java
@@ -2,9 +2,13 @@ package org.bondarenko.atm;
 
 public class Screen {
 	private static String message ;
-	public static void showMenu(){}
+	public static void showMenu(){
+		System.out.println("ATM menu");
+	}
 	public static void showAccountStatus(long accountNumber, double sum){}
-	public static void showMessage(){}
+	public static void showMessage(){
+		System.out.println(message);
+	}
 	public static void setMessage(String mess){
 		message = mess;
 	}
