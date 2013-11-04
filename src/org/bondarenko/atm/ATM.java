caec385caea0af0b9764a8e33136c64ac6adccac
Bondarenko Katya <karameilka@ukr.net>
2013-11-04 10:08:52 +0200

    class ATM was updated

--- src/org/bondarenko/atm/ATM.java
+++ src/org/bondarenko/atm/ATM.java
@@ -1,21 +1,15 @@
 package org.bondarenko.atm;
 
 public class ATM {
-	private int cardId;
-	private int pinCode; 
-	private int sum ;
-
+	
 	public static void main(String[] args) {
 		
 
 	}
 	
-	public short readPINCod()
-	public boolean checkPIN()
-	public boolean checkIDcard()
-	public void createTransaction()
-	public void completeTransaction()
-	public Boolean hasNecessarySum()
-	public void setSum(int sum)
+	public void StartWork(){}
+	public void completeWork(){}
+	
+	
 
 }
